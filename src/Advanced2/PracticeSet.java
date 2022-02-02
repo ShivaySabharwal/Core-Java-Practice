bc5064452597036405518e09ac1ba92d3e3a5d78
Shivay Sabharwal <shivaysabharwal.cse@gmail.com>
2022-02-02 21:07:28 +0530

    Added Advanced2 Practice Set

--- src/Advanced2/PracticeSet.java
+++ src/Advanced2/PracticeSet.java
@@ -1,15 +1,45 @@
 package Advanced2;
 
+import java.io.FileWriter;
+import java.io.IOException;
+
 class MyDeprecated{
     @Deprecated
     void meth1(){
         System.out.println("Hello Java");
     }
 }
+
+interface MyInt{
+    void display();
+}
+
 public class PracticeSet {
     public static void main(String[] args) {
         @SuppressWarnings("Deprecation")
         MyDeprecated a = new MyDeprecated();
         a.meth1();
+
+        MyInt obj = new MyInt() {
+            @Override
+            public void display() {
+                System.out.println("I am in Display Method.");
+            }
+        };
+
+        String table = "";
+        for(int i = 0; i <= 10; i++){
+            for (int j = 0; j < 10; j++) {
+                table += i + "X"+(j+1) + " = " + i*(j+1);
+                table += "\n";
+            }
+            try {
+                FileWriter fileWriter = new FileWriter("MultiplicationTable.txt");
+                fileWriter.write(table);
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
